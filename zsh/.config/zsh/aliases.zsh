9a153896f2f0dbe419525aecea9ce5d8cffbc943
Aldo Morua <aldo.morua@lifelinksystems.com>
2023-07-03 19:41:24 -0600

    Adding aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -30,7 +30,26 @@ alias vsc="code ."
 #LIFELINK
 alias mfa='~/lifelink/ops/mfa.sh'
 alias startAll='cd ~/lifelink/getstarted/; ./startAll.sh'
+alias author='cd ~/lifelink/author'
+alias pantry='cd ~/lifelink/pantry'
+alias electro='cd ~/lifelink/electro'
+alias integra='cd ~/lifelink/integra'
+alias stanza='cd ~/lifelink/stanza'
+alias cookbook='cd ~/lifelink/cookbook'
+alias toolshed='cd ~/lifelink/toolshed'
+alias sapiv5='cd ~/lifelink/services-api-grails5'
+alias sapi='cd ~/lifelink/services-api'
+alias aspen='cd ~/lifelink/aspen'
+alias fakeAviator='cd ~/lifelink/fake-aviator'
+alias vail='cd ~/lifelink/vail'
+alias fakeAuth='cd ~/lifelink/fake-auth-service'
+alias e2e='cd ~/lifelink/e2e-tests'
+alias test-space='cd ~/Documents/tests'
 
 alias gh-pr-vail="gh pr create -a @me -B develop -r devtrillo,Dazuam"
 alias gh-pr-platform="gh pr create -a "@me" -B master -r devtrillo,azimmer,bryan-lifelink,justinwiley,lifelinkpaul,monkeysaa,shoshanaedelman"
+alias gh-pr-implementation="gh pr create -a "@me" -B master -r alexisLael,freddyiniguez-2,psicobloc,Dazuam,JulianRev"
 
+#Command line
+alias cl="clear"
+alias edit-aliases="code ~/dotfiles/zsh/.config/zsh/aliases.zsh"
